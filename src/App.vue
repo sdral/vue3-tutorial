8ef44a30cf4af89eb8f079387a51351f0ff7fe42
Lim Mun Xuan <86650860+sdral@users.noreply.github.com>
2022-02-07 18:44:41 +0800

    vue_ex1_1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< Updated upstream
   <br />
 
   Add 1 :<button @click="increase"> +1 </button>   <br />
@@ -23,6 +24,36 @@
   Name: {{ name }} <br />
   Addr: {{ address }}<br />
   Tel : {{ tel }} <br />
+=======
+  {{ message }} <br />
+
+  Add: <button @click="increase">+1</button> <br />
+  Minus: <button @click="decrease">-1</button> <br />
+
+  Add+5: <button @click="increaseFive">+5</button> <br />
+  Minus-5: <button @click="decreaseFive">-5</button> <br />
+
+  <h2>{{ counter }}</h2>
+
+  Name: <input type="text" v-model="name" /><br />
+
+  Address: <input type="text" v-model="address" /><br />
+
+  Tel: <input type="text" v-model="tel" /><br />
+
+  Email: <input type="text" v-model="email" /><br />
+  <br />
+
+  <img v-bind:src="filename" /><br />
+  <img v-bind:src="img1" /><br />
+  <img v-bind:src="img2" /><br />
+
+  <hr />
+
+  Name: {{ name }} <br />
+  Address: {{ address }} <br />
+  Tel: {{ tel }} <br />
+>>>>>>> Stashed changes
   Email: {{ email }} <br />
 </template>
 
@@ -35,13 +66,19 @@ export default {
       tel: "",
       email: "",
       filename: "logo.png",
+<<<<<<< Updated upstream
       image1: "apple.jpg",
       image2: "orange.jpg",
+=======
+      img1: "minion1.jpg",
+      img2: "minion2.jpg",
+>>>>>>> Stashed changes
       counter: 0,
     };
   },
   components: {},
   methods: {
+<<<<<<< Updated upstream
       increase() {
           this.counter = this.counter + 1
       },
@@ -49,6 +86,21 @@ export default {
           this.counter = this.counter - 1
       }
   }
+=======
+    increase() {
+      this.counter = this.counter + 1;
+    },
+    decrease() {
+      this.counter = this.counter - 1;
+    },
+    increaseFive() {
+      this.counter = this.counter + 5;
+    },
+    decreaseFive() {
+      this.counter = this.counter - 5;
+    },
+  },
+>>>>>>> Stashed changes
 };
 </script>
 
