4a51a06c7e6e3ec7e0419c85fc0801c0eb01b696
Lim Mun Xuan <86650860+sdral@users.noreply.github.com>
2022-02-07 18:47:08 +0800

    vue_ex1_1.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-  <br />
-
-  Add 1 :<button @click="increase"> +1 </button>   <br />
-  Minus 1 :<button @click="decrease"> -1 </button>  <br />
-  <br>
-  Add 5 :<button @click="increase"> +5 </button>   <br />
-  Minus 5 :<button @click="decrease"> -5 </button>  <br />
-
-  <h2>{{ counter }} </h2>
-
-
-  Name: <input type="text" v-model="name" /> <br />
-  Address: <input type="text" v-model="address" /> <br />
-  Tel : <input type="text" v-model="tel" /> <br />
-  email : <input type="text" v-model="email" /> <br />
-
-  <img v-bind:src="filename" /> <br />
-  <img :src="image1" /> <br />
-  <img :src="image2" /> <br />
-
-  <hr />
-  Name: {{ name }} <br />
-  Addr: {{ address }}<br />
-  Tel : {{ tel }} <br />
-=======
   {{ message }} <br />
 
   Add: <button @click="increase">+1</button> <br />
@@ -53,7 +27,6 @@
   Name: {{ name }} <br />
   Address: {{ address }} <br />
   Tel: {{ tel }} <br />
->>>>>>> Stashed changes
   Email: {{ email }} <br />
 </template>
 
@@ -66,27 +39,13 @@ export default {
       tel: "",
       email: "",
       filename: "logo.png",
-<<<<<<< Updated upstream
-      image1: "apple.jpg",
-      image2: "orange.jpg",
-=======
       img1: "minion1.jpg",
       img2: "minion2.jpg",
->>>>>>> Stashed changes
       counter: 0,
     };
   },
   components: {},
   methods: {
-<<<<<<< Updated upstream
-      increase() {
-          this.counter = this.counter + 1
-      },
-      decrease() {
-          this.counter = this.counter - 1
-      }
-  }
-=======
     increase() {
       this.counter = this.counter + 1;
     },
@@ -100,7 +59,6 @@ export default {
       this.counter = this.counter - 5;
     },
   },
->>>>>>> Stashed changes
 };
 </script>
 
